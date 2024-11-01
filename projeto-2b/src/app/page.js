bf29d5131b997f3a8da02b7ef999daffbf743404
josesbaraini <josesbarainips@gmail.com>
2024-11-01 14:23:19 -0400

    Nooooosa

---
+++ projeto-2b/src/app/page.js
@@ -0,0 +1,11 @@
+
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>Meu Titulo</h1>
+      <input type="text" content="" />
+    </div>
+  );
+}
