bc030d17d50062a5389b90be17a78ccfd254fd3f
josesbaraini <josesbarainips@exemplo.br>
2024-12-06 14:00:53 -0400

    oli

--- projeto-2b/src/app/props/page.js
+++ projeto-2b/src/app/props/page.js
@@ -1,13 +1,45 @@
 import styles from "./props.module.css";
+import Card from "@/components/Card"
+const carros =[
+    {id: 1,
+        modelo:'gol',
+        ano: 2000,
+    
+        cor:'azul'
+    },
+    {id: 2,
+        modelo:'celta',
+        ano: 2020,
+    
+        cor:'preto'
+    },
+    
+    {id: 3,
+    modelo:'fusca',
+    ano: 1999,
 
+    cor:'azul'
+    }
+
+
+]
+    
 
 
 export default function Props() {
   return (
     <div>
       <h1>Props</h1>
+      {carros.map((carro)=>( 
+      <Card
+      key={carro.id}
+      id={carro.id}
+      modelo={carro.modelo}
+      ano={carro.ano}
+      cor={carro.cor}
+      />
 
-      
+    ))}  
     </div>
   );
 }
--- projeto-2b/src/components/Card/index.js
+++ projeto-2b/src/components/Card/index.js
@@ -4,6 +4,8 @@ export default function Card(props) {
     return (
       <div>
         <h1>{props.id} - {props.modelo}</h1>
+        <p>{props.ano}</p>
+        <p>{props.cor}</p>
   
         
       </div>
