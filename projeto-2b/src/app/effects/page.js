e90c9a4cfb504691b013eed932347815cf3a7644
José Sbaraini <josesbarainips@gmail.com>
2024-12-20 23:28:55 -0400

    coisas

--- projeto-2b/src/app/effects/page.js
+++ projeto-2b/src/app/effects/page.js
@@ -1,49 +1,65 @@
 'use client'
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import styles from "./page.module.css";
 
 
 const coisas = () =>{
-    let [ufs, setUfs] = useState([])
-    let [estado, setEstado] = useState('')
+    // let [ufs, setUfs] = useState([])
+    // let [estado, setEstado] = useState('')
 
-    let [cidades, setCidades] = useState([])
-    let [cidade, setCidade] = useState('')
-    const getUfs = async () =>{
-        const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome');
-        if (!response.ok) {
-            throw new Error('Deu rum buscando os dados'+ response.statusText);
-            }
-        const data = await response.json();
-        setUfs(data)
-        console.log(data);
+    // let [cidades, setCidades] = useState([])
+    // let [cidade, setCidade] = useState('')
+    // const getUfs = async () =>{
+    //     const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome');
+    //     if (!response.ok) {
+    //         throw new Error('Deu rum buscando os dados'+ response.statusText);
+    //         }
+    //     const data = await response.json();
+    //     setUfs(data)
+    //     console.log(data);
 
+    // }
+
+    // const getCidades = async () =>{
+    //     const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios?orderBy=nome`);
+    //     if (!response.ok) {
+    //         throw new Error('Deu rum buscando os dados'+ response.statusText);
+    //         }
+    //     const data = await response.json();
+    //     setCidades(data)
+    //     console.log(estado);
+
+    // };
+    // useEffect(()=>{
+    //     getUfs();
+    // },[]);
+
+    // useEffect(()=>{
+    //     getCidades()
+    // },[estado]);
+    let [poder , setPoder] = useState(0)
+    const manipularPoder = (evento) =>{
+
+        setPoder(Number(evento.target.value))
     }
-
-    const getCidades = async () =>{
-        const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios?orderBy=nome`);
-        if (!response.ok) {
-            throw new Error('Deu rum buscando os dados'+ response.statusText);
-            }
-        const data = await response.json();
-        setCidades(data)
-        console.log(estado);
-
-    };
-    useEffect(()=>{
-        getUfs();
-    },[]);
-
-    useEffect(()=>{
-        getCidades()
-    },[estado]);
 
 
 
 
     return (
-        <main>
-            <select onChange={(ev)=>setEstado(ev.target.value)}>
+        <main className={styles.main}>
+                <div className={styles.div}>
+                <input className={styles.input} type="text" onChange={manipularPoder} />
+                <p className={styles.texto}>Dano: {8 +(2*poder)}d8 + { 5*(4+poder)} </p>
+                <p className={styles.texto}>Critico: {20 -(poder*2)} ou mais </p>
+                <p className={styles.texto}>Dano critico: {(8 +(2*poder))*(2+poder)}d8 + { 5*(4+poder)}  </p>
+                <p className={styles.texto}>Acerto: 1d20 + 15 ou 19 ou 21</p>
+                </div>
+            
+            
+            
+            {/* <select onChange={(ev)=>setEstado(ev.target.value)}>
                 <option value=''>Selecione o Estado</option>
                 {ufs.map((uf)=>(
                     <option
@@ -66,7 +82,9 @@ const coisas = () =>{
                     </option>
                 ))}
 
-            </select>
+            </select> */}
+
+            
 
 
         </main>
