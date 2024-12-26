4183f4586836650f9ba3b6aec53efabdc3fba6c3
José Sbaraini <josesbarainips@gmail.com>
2024-12-26 18:50:20 -0400

    ol

---
+++ projeto-2b/src/components/CardFicha/index.js
@@ -0,0 +1,13 @@
+import styles from "./Card.module.css";
+
+export default function CardF(props) {
+    return (
+      <div>
+        <p></p>
+        <p></p>
+        <p></p>
+  
+        
+      </div>
+    );
+  }
